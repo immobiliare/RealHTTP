1e60d0f7c16f3622980466e53128b1c0b680f6c7
Daniele Margutti <hello@danielemargutti.com>
2022-02-03 11:26:30 +0100

    Added the hTTPAltRequestValidator

--- Sources/RealHTTP/Client/Internal/HTTPResponseValidator/HTTPAltRequestValidator.swift
+++ Sources/RealHTTP/Client/Internal/HTTPResponseValidator/HTTPAltRequestValidator.swift
@@ -18,35 +18,46 @@ import Foundation
 /// This validator can be used to provide an alternate request operation to execute before
 /// retry the initial request. It may be used to provide silent login operation when you receive
 /// and authorized/forbidden errors.
+///
 /// It's triggered by the `triggerHTTPCodes` which by default is set `.unathorized, .forbidden`.
 open class HTTPAltRequestValidator: HTTPValidator {
     public typealias RetryRequestProvider = ((_ request: HTTPRequest, _ response: HTTPResponse) -> HTTPRequest?)
     
-    // MARK: - Public Properties
+    // MARK: - Public Properties (Configuration Options)
     
-    /// Trigger codes.
-    open var triggerHTTPCodes: Set<HTTPStatusCode>
+    /// HTTP status codes which trigger the validator callback.
+    ///
+    /// NOTE:
+    /// If you want to trigger the no-response (ie. network failure) you should
+    /// add `none` in triggered status code response.
+    open var statusCodes: Set<HTTPStatusCode>
     
-    /// Provider of the request.
-    open var requestProvider: RetryRequestProvider
+    /// This is the delay to retry the initial request after the alt request
+    /// has been executed. By default is 0s.
+    open var retryDelay: TimeInterval = 0
     
-    /// This allows you to observe the response of the alt request executed
-    /// and perform your own logic before retry the initial call failed.
+    /// Number of alternate calls to execute.
+    /// By default is set to 1.
+    /// It means the first alternate call which fails on a certain
+    /// request will fails any other request in the same session.
+    open var maxAltRequests = 1
+    
+    // MARK: - Public Properties (Observable Events)
+    
+    /// This callback is called when validator ask to you to provide the
+    /// alternate request to execute before retry the initial failed call.
+    open var onProvideAltRequest: RetryRequestProvider
+    
+    /// This callback is called when the alternate request did finish and
+    /// you want to read and so something with the response.
+    /// This is an async call you can use to perform your own logic before
+    /// the initial call is retried automatically.
     ///
     /// For example if your strategy is to try a silent login in your
     /// alt request you can use this method setup received response with
     /// the token as your session cookies so when the initial call is retried
     /// everything will be okay.
-    open var altRequestCatcher: HTTPRetryStrategy.AltRequestCatcher?
-    
-    /// This is the delay to retry the initial request after the alt request
-    /// has been executed. By default is 0s.
-    open var retryMainCallDelay: TimeInterval = 0
-    
-    /// Number of alternate calls to execute.
-    /// By default is set to 1. It means the first alternate call which fails on a certain
-    /// request will fails any other request in the same session.
-    open var maxAltRequestsToExecute: Int? = nil
+    open var onReceiveAltResponse: HTTPRetryStrategy.AltRequestCatcher?
     
     // MARK: - Private Properties
     
@@ -55,17 +66,22 @@ open class HTTPAltRequestValidator: HTTPValidator {
     
     // MARK: - Initialization
     
-    /// Initialize to provide a new request when a code is triggered.
-    /// The alternate request is executed in case of triggered errors, then the original request is re-executed.
+    /// Initialize to provide a new request when one of `triggeredCodes` arrives.
     ///
     /// - Parameters:
-    ///   - triggerCodes: trigger http codes, by default `[.unauthorized, .forbidden]`.
-    ///   - requestProvider: provider of the alternate request to execute.
-    public init(triggerCodes: Set<HTTPStatusCode> = [.unauthorized, .forbidden],
-                _ requestProvider: @escaping RetryRequestProvider) {
-        self.triggerHTTPCodes = triggerCodes
-        self.requestProvider = requestProvider
+    ///   - statusCodes: array of `HTTPStatusCode` which trigger the validator (default is `[.unauthorized, .forbidden]`)
+    ///   - onProvideAltRequest: the function which ask what alternate request you want
+    ///                          to execute when a initial request is received
+    ///   - onReceiveAltResponse: use this callback to catch the response received by the alt request and perform your logic.
+    public init(statusCodes: Set<HTTPStatusCode> = [.unauthorized, .forbidden],
+                onProvideAltRequest: @escaping RetryRequestProvider,
+                onReceiveAltResponse: HTTPRetryStrategy.AltRequestCatcher?) {
+        self.statusCodes = statusCodes
+        self.onProvideAltRequest = onProvideAltRequest
+        self.onReceiveAltResponse = onReceiveAltResponse
     }
+    
+    // MARK: - Public Functions
     
     /// Reset the state of alt requests executed.
     public func reset() {
@@ -75,25 +91,25 @@ open class HTTPAltRequestValidator: HTTPValidator {
     // MARK: - Protocol Conformance
     
     public func validate(response: HTTPResponse, forRequest request: HTTPRequest) -> HTTPResponseValidatorResult {
-        if let statusCode = response.error?.statusCode, triggerHTTPCodes.contains(statusCode) {
+        guard triggerHTTPCodes.contains(response.statusCode) else {
+            // if received status code for this request is not inside the triggerable status codes we'll skip the validator.
+            return .nextValidator
+        }
+                
+        numberOfAltRequestExecuted += 1
+        
+        if numberOfAltRequestExecuted > maxAltRequests {
+            // If we reached the maximum number of alternate calls to execute we want to cancel any other attempt.
+            return .failChain(HTTPError(.maxRetryAttemptsReached))
+        }
+
+        // if no retry operation is provided we'll skip and mark the validation as passed
+        guard let altOperation = onProvideAltRequest(request, response) else {
             return .nextValidator
         }
         
-        // If error is one of the errors in `triggerHTTPCodes`
-        
-        // If we reached the maximum number of alternate calls to execute we want to cancel any other attempt.
-        numberOfAltRequestExecuted += 1
-        if let maxAltRequestsToExecute = maxAltRequestsToExecute,
-              numberOfAltRequestExecuted > maxAltRequestsToExecute {
-            let error = HTTPError(.maxRetryAttemptsReached)
-            return .failChain(error)
-        }
-        
-        guard let altOperation = requestProvider(request, response) else {
-            return .nextValidator // if no retry operation is provided we'll skip and mark the validation as passed
-        }
-        
-        return .retry(.after(altOperation, retryMainCallDelay, altRequestCatcher))
+        // Perform the alt request strategy.
+        return .retry(.after(altOperation, retryDelay, onReceiveAltResponse))
     }
     
 }
