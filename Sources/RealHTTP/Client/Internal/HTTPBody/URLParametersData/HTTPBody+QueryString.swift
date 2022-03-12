2758a6021acaba6fadce3560a8f79b48f19658bc
Daniele Margutti <hello@danielemargutti.com>
2022-03-12 11:19:53 +0100

    new #18: deprecated urlParameters() to formURLEncodedBody()

--- Sources/RealHTTP/Client/Internal/HTTPBody/URLParametersData/HTTPBody+QueryString.swift
+++ Sources/RealHTTP/Client/Internal/HTTPBody/URLParametersData/HTTPBody+QueryString.swift
@@ -19,20 +19,56 @@ public typealias HTTPRequestParametersDict = [String: Any]
 
 extension HTTPBody {
     
+    /// Deprecated, see `formURLEncodedBody`.
+    ///
+    /// - Parameter parameters: parameters to set.
+    /// - Returns: HTTPBody
+    @available(*, deprecated, message: "Use formURLEncodedBody instead")
+    public static func urlParameters(_ parameters: HTTPRequestParametersDict) -> HTTPBody {
+        formURLEncodedBody(parameters)
+    }
+    
     /// Create a new body which contains the query string with passed parameters.
     ///
     /// - Parameter parameters: parameters.
     /// - Returns: HTTPBody
-    public static func urlParameters(_ parameters: HTTPRequestParametersDict) -> HTTPBody {
-        let content = URLParametersData(parameters)
-        var body = HTTPBody(content: content, headers: .init())
+    public static func formURLEncodedBody(_ parameters: HTTPRequestParametersDict) -> HTTPBody {
+        let content = WWWFormURLEncodedBody(parameters)
+        let body = HTTPBody(content: content, headers: [
+            .contentType(MIMEType.wwwFormUtf8.rawValue)
+        ])
+        return body
+    }
+    
+}
+
+// MARK: - WWWFormURLEncoded
+
+extension HTTPBody {
+    
+    /// Encode the parameters inside the body with standard url encoding.
+    public struct WWWFormURLEncodedBody: HTTPSerializableBody {
         
-        if let data = try? content.encodedData() {
-            body.headers[.contentType] = MIMEType.wwwFormUtf8.rawValue
-            body.headers[.contentLength] = String(data.count)
+        // MARK: - Public Properties
+        
+        /// Parameters to set with url encoded body.
+        public let parameters: URLParametersData
+        
+        // MARK: - Initialziation
+        
+        /// Initialize a new body with parameters dictionary.
+        ///
+        /// - Parameter parameters: parameters.
+        public init(_ parameters: HTTPRequestParametersDict) {
+            self.parameters = URLParametersData(parameters)
         }
         
-        return body
+        // MARK: - HTTPSerializableBody Conformance
+        
+        public func serializeData() async throws -> (data: Data, additionalHeaders: HTTPHeaders?) {
+            try await parameters.serializeData()
+        }
+        
     }
     
 }
