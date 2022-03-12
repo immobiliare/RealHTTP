d5e3178951db236c71fe96777f1904cba62f204e
Daniele Margutti <hello@danielemargutti.com>
2022-03-12 11:12:30 +0100

    new #18: Making the cURLDescription available for HTTPResponse. for HTTPRequest is now async.

--- Sources/RealHTTP/Client/Internal/Other Structures/cURLHelper.swift
+++ Sources/RealHTTP/Client/Internal/Other Structures/cURLHelper.swift
@@ -15,31 +15,64 @@
 
 import Foundation
 
+// MARK: - cURL for HTTPRequest
+
 extension HTTPRequest {
     
-    /// Return the cURL representation of the request when loaded
-    /// inside a specific client.
+    /// Return the cURL representation of the origin request which
+    /// originate this response.
     ///
-    /// - Parameter client: client where the request should run.
+    /// NOTE:
+    ///
+    /// Parent client must be exists in order to get valid results.
+    /// This is an asynchronous call because it needs to serialize data and
+    /// it's asynchronous operation.
+    /// You can use the `HTTPResponse`'s `curlDescription()` to have a sync
+    /// response for an executed request.
+    ///
     /// - Returns: String
-    public func cURLDescription(whenIn client: HTTPClient?) -> String {
+    func cURLDescription(whenIn client: HTTPClient?) async throws -> String {
+        let request = try await urlRequest(inClient: client)
+        return cURLHelper.cURLDescription(request: request, client: client)
+    }
+    
+}
+
+// MARK: - cURL for HTTPResponse
+
+extension HTTPResponse {
+    
+    /// Return the cURL description for origin request who generated this response.
+    ///
+    /// - Returns: String
+    func cURLDescription() -> String {
+        cURLHelper.cURLDescription(request: request?.sessionTask?.originalRequest, client: request?.client)
+    }
+    
+}
+
+// MARK: - cURLHelper
+
+/// Contains the relevant information to print cURL description for requests and responses.
+public struct cURLHelper {
+    
+    fileprivate static func cURLDescription(request: URLRequest?, client: HTTPClient?) -> String {
         guard let client = client,
-              let request = try? urlRequest(inClient: client),
-              let _ = request.url,
-              let method = request.httpMethod else {
-            return "$ curl command could not be created"
-        }
+              let request = request,
+              let _ = request.url else {
+                  return "$ curl command could not be created"
+              }
         
         var components = [
             "$ curl -v"
         ]
-
-        components += "-X \(method)"
-
-        addCredentials(for: request, whenIn: client, into: &components)
-        addSetCookies(for: request, whenIn: client, into: &components)
-        addHeaders(for: request, whenIn: client, into: &components)
-        addBody(for: request, whenIn: client, into: &components)
+        
+        components += "-X \(request.httpMethod ?? "")"
+        
+        cURLHelper.addCredentials(for: request, whenIn: client, into: &components)
+        cURLHelper.addSetCookies(for: request, whenIn: client, into: &components)
+        cURLHelper.addHeaders(for: request, whenIn: client, into: &components)
+        cURLHelper.addBody(for: request, whenIn: client, into: &components)
         
         return components.joined(separator: " \\\n\t")
     }
@@ -52,19 +85,19 @@ extension HTTPRequest {
     ///   - request: request.
     ///   - client: where the request is running in.
     ///   - components: components array.
-    private func addCredentials(for request: URLRequest, whenIn client: HTTPClient, into components: inout [String]) {
+    private static func addCredentials(for request: URLRequest, whenIn client: HTTPClient, into components: inout [String]) {
         guard let credentialStorage = client.session.configuration.urlCredentialStorage,
               let url = request.url,
               let host = url.host else {
-            return
-        }
+                  return
+              }
         
         let protectionSpace = URLProtectionSpace(host: host,
                                                  port: url.port ?? 0,
                                                  protocol: url.scheme,
                                                  realm: host,
                                                  authenticationMethod: NSURLAuthenticationMethodHTTPBasic)
-
+        
         if let credentials = credentialStorage.credentials(for: protectionSpace)?.values {
             for credential in credentials {
                 guard let user = credential.user, let password = credential.password else { continue }
@@ -79,18 +112,18 @@ extension HTTPRequest {
     ///   - request: request.
     ///   - client: where the request is running in.
     ///   - components: components array.
-    private func addSetCookies(for request: URLRequest, whenIn client: HTTPClient, into components: inout [String]) {
+    private static func addSetCookies(for request: URLRequest, whenIn client: HTTPClient, into components: inout [String]) {
         let configuration = client.session.configuration
         guard configuration.httpShouldSetCookies,
               let url = request.url else {
-            return
-        }
+                  return
+              }
         
         if
             let cookieStorage = configuration.httpCookieStorage,
             let cookies = cookieStorage.cookies(for: url), !cookies.isEmpty {
             let allCookies = cookies.map { "\($0.name)=\($0.value)" }.joined(separator: ";")
-
+            
             components.append("-b \"\(allCookies)\"")
         }
         
@@ -102,7 +135,7 @@ extension HTTPRequest {
     ///   - request: request.
     ///   - client: where the request is running in.
     ///   - components: components array.
-    private func addHeaders(for request: URLRequest, whenIn client: HTTPClient, into components: inout [String]) {
+    private static func addHeaders(for request: URLRequest, whenIn client: HTTPClient, into components: inout [String]) {
         let configuration = client.session.configuration
         var headers = HTTPHeaders()
         
@@ -126,15 +159,15 @@ extension HTTPRequest {
     ///   - request: request.
     ///   - client: where the request is running in.
     ///   - components: components array.
-    private func addBody(for request: URLRequest, whenIn client: HTTPClient, into components: inout [String]) {
+    private static func addBody(for request: URLRequest, whenIn client: HTTPClient, into components: inout [String]) {
         if let httpBodyData = request.httpBody {
             let httpBody = String(decoding: httpBodyData, as: UTF8.self)
             var escapedBody = httpBody.replacingOccurrences(of: "\\\"", with: "\\\\\"")
             escapedBody = escapedBody.replacingOccurrences(of: "\"", with: "\\\"")
-
+            
             components.append("-d \"\(escapedBody)\"")
         }
-
+        
         components.append("\"\(request.url!.absoluteString)\"")
     }
     
