9a13c55e02dc746fbd2e77472ca545f74894cecb
Daniele Margutti <hello@danielemargutti.com>
2022-09-23 11:54:39 +0200

    Bump to 1.7.2

--- Sources/RealHTTP/RealHTTP.swift
+++ Sources/RealHTTP/RealHTTP.swift
@@ -18,7 +18,7 @@ import Foundation
 public enum RealHTTP {
     
     /// Current RealHTTP version.
-    static let sdkVersion = "1.7.1"
+    static let sdkVersion = "1.7.2"
     
     /// Identifier of the agent string.
     static let agentIdentifier = "realhttp"
